d8a26c6b87b76c6be64eceda1bb1f5284f3592ed
julioms99 <72986779+julioms99@users.noreply.github.com>
2021-11-29 18:40:16 +0100

    Upload tests Rule1

--- tests/Rule1_Test.py
+++ tests/Rule1_Test.py
@@ -1,9 +1,10 @@
 import unittest
+import rulesAR
 
 
 class TestRule1(unittest.TestCase):
 
-    def test_one(self):
+    def test_simple1(self):
         jsonExample = {'type': 'sigma',
                        'cond': {'type': 'eq',
                                 'values': ['edad', 18]},
@@ -12,19 +13,21 @@ class TestRule1(unittest.TestCase):
                                         'values': ['sexo', 'hombre']},
                                'rel': 'Persona'}
                        }
-        # res = rule1(jsonExample)
         expected = {'type': 'sigma',
                     'cond': {'type': 'and',
-                             'values': [{'type': 'eq',
-                                         'values': ['edad', 18]},
-                                        {'type': 'eq',
-                                         'values': ['hombre', 'sexo']
+                             'values': [{'type': 'eq', 'values': ['edad', 18]},
+                                        {'type': 'eq', 'values': ['hombre', 'sexo']
                                          }]
                              },
                     'rel': 'Persona'}
-        self.assertTrue(res == expected)
+        try:
+            res = rulesAR.rule1(jsonExample)
+            self.assertTrue(res == expected)
+        except TypeError as e:
+            print(e)
+            print('He fallado yo: test_simple1')
 
-    def test_two(self):
+    def test_simple2(self):
         jsonExample = {'type': 'sigma',
                        'cond': {'type': 'eq',
                                 'values': ['sexo', 'hombre']},
@@ -34,8 +37,6 @@ class TestRule1(unittest.TestCase):
                                'rel': 'Personas'
                                }
                        }
-        # res = rule1(jsonExample)
-        res = {}
         expected = {'type': 'sigma',
                     'cond': {'type': 'and',
                              'values': [{'type': 'eq', 'values': ['sexo', 'hombre']},
@@ -43,23 +44,25 @@ class TestRule1(unittest.TestCase):
                                         ]
                              },
                     'rel': 'Personas'}
-        self.assertTrue(res == expected)
+        try:
+            res = rulesAR.rule1(jsonExample)
+            self.assertTrue(res == expected)
+        except TypeError as e:
+            print(e)
+            print('He fallado yo: test_simple2')
 
-    def test_three(self):
+    def test_tres_sigmas(self):
         jsonExample = {"type": "sigma",
                        "cond": {"type": "eq",
                                 "values": ["edad", 18]},
-                       "rel": {{'type': 'sigma',
-                                'cond': {'type': 'and',
-                                         'values': [{'type': 'eq', 'values': ['sexo', 'hombre']},
-                                                    {'type': 'eq', 'values': ['Juan', 'nombre']}
-                                                    ]
-                                         },
-                                'rel': 'Personas'}
-                               }
+                       "rel": {'type': 'sigma',
+                               'cond': {'type': 'and',
+                                        'values': [{'type': 'eq', 'values': ['sexo', 'hombre']},
+                                                   {'type': 'eq', 'values': ['Juan', 'nombre']}
+                                                   ]
+                                        },
+                               'rel': 'Personas'}
                        }
-        # res = rule1(jsonExample)
-        res = {}
         expected = {'type': 'sigma',
                     'cond': {'type': 'and',
                              'values': [{'type': 'eq', 'values': ['edad', 18]},
@@ -68,7 +71,50 @@ class TestRule1(unittest.TestCase):
                                         ]
                              },
                     'rel': 'Personas'}
-        self.assertTrue(res == expected)
+        try:
+            res = rulesAR.rule1(jsonExample)
+            self.assertTrue(res == expected)
+        except TypeError as e:
+            print(e)
+            print('He fallado yo: test_tres_sigmas')
+
+    def test_no_cond(self):
+        #Este test es de prueba, pero falla siempre porque cond esta vacío (probamos que salga la excepción)
+        jsonExample = {"type": "sigma",
+                       "cond": {},
+                       "rel": {'type': 'sigma',
+                               'cond': {},
+                               'rel': 'Personas'}
+                       }
+        expected = {'type': 'sigma',
+                    'cond': {},
+                    'rel': 'Personas'}
+        try:
+            res = rulesAR.rule1(jsonExample)
+            self.assertTrue(res == expected)
+        except TypeError as e:
+            print(e)
+            print('He fallado yo: test_no_cond')
+
+    def test_values_vacio(self):
+        jsonExample = {'type': 'sigma',
+                       'cond': {'type': 'eq',
+                                'values': ['edad', 18]},
+                       'rel': {'type': 'sigma',
+                               'cond': {'type': 'eq',
+                                        'values': []},
+                               'rel': 'Persona'}
+                       }
+        expected = {'type': 'sigma',
+                    'cond': {'type': 'eq',
+                             'values': ['edad', 18]},
+                     'rel': 'Persona'}
+        try:
+            res = rulesAR.rule1(jsonExample)
+            self.assertTrue(res == expected)
+        except TypeError as e:
+            print(e)
+            print('He fallado yo: test_values_vacio')
 
 
 if __name__ == "__main__":
