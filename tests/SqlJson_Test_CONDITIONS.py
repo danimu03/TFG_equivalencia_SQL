e834482a148cc033712f1dc8a2113c0287565f1e
sergiova97 <49974534+sergiova97@users.noreply.github.com>
2021-12-06 16:55:50 +0100

    Tests documentados

--- tests/SqlJson_Test_CONDITIONS.py
+++ tests/SqlJson_Test_CONDITIONS.py
@@ -2,8 +2,34 @@ import unittest
 from src.SQL_To_JSON import Sql_To_Json as sqlJSON
 
 class SqlJson_Test_CONDITIONS(unittest.TestCase):
+    """
+    Class to test transformations from SQL to relational algebra, focusing on conditions
 
-    def test_equalString (self):
+    Methods
+    _______
+    test_equalString
+        Test of equal to String
+
+    test_equalInt
+        Test of equal to Int
+
+    test_ANDTwoEquals
+        Test of equal AND equal
+
+    test_ANDThreeEquals
+        Test of two AND and three equals
+
+    test_ANDandVariousSelects
+        Test of two AND and SELECT various attributes
+    """
+
+    def test_equalString(self):
+        """
+        Test of equal to String
+
+        Original query:  SELECT Nombre FROM Persona WHERE Pais = \"España\"
+        """
+
         res = sqlJSON.parse_Sql_To_Json("SELECT Nombre FROM Persona WHERE Pais = \"España\"")
         expected = {"type": "pi",
                     "proj": ["Nombre"],
@@ -19,6 +45,12 @@ class SqlJson_Test_CONDITIONS(unittest.TestCase):
         self.assertEqual(res, expected)
 
     def test_equalInt(self):
+        """
+        Test of equal to Int
+
+        Original query:  SELECT Nombre FROM Persona WHERE Telefono = 12345"
+        """
+
         res = sqlJSON.parse_Sql_To_Json("SELECT Nombre FROM Persona WHERE Telefono = 12345")
         expected = {"type": "pi",
                     "proj": ["Nombre"],
@@ -34,6 +66,12 @@ class SqlJson_Test_CONDITIONS(unittest.TestCase):
         self.assertEqual(res, expected)
 
     def test_ANDTwoEquals(self):
+        """
+        Test of equal AND equal
+
+        Original query:  SELECT Nombre FROM Persona WHERE Pais = \"España\" AND Telefono = 12345
+        """
+
         res = sqlJSON.parse_Sql_To_Json("SELECT Nombre FROM Persona WHERE Pais = \"España\" AND Telefono = 12345")
         expected = {"type": "pi",
                     "proj": ["Nombre"],
@@ -52,6 +90,12 @@ class SqlJson_Test_CONDITIONS(unittest.TestCase):
         self.assertEqual(res, expected)
 
     def test_ANDThreeEquals(self):
+        """
+        Test of two AND and three equals
+
+        Original query:  SELECT Nombre FROM Persona WHERE Pais = \"España\" AND Telefono = 12345 AND Id = \"IS1452\"
+        """
+
         res = sqlJSON.parse_Sql_To_Json("SELECT Nombre FROM Persona WHERE Pais = \"España\" AND Telefono = 12345 AND Id = \"IS1452\"")
         expected = {"type": "pi",
                     "proj": ["Nombre"],
@@ -72,6 +116,12 @@ class SqlJson_Test_CONDITIONS(unittest.TestCase):
         self.assertEqual(res, expected)
 
     def test_ANDandVariousSelects(self):
+        """
+        Test of two AND and SELECT various attributes
+
+        Original query:  SELECT Nombre, Edad FROM Persona WHERE Pais = \"España\" AND Telefono = 12345 AND Id = \"IS1452\"
+        """
+
         res = sqlJSON.parse_Sql_To_Json("SELECT Nombre, Edad FROM Persona WHERE Pais = \"España\" AND Telefono = 12345 AND Id = \"IS1452\"")
         expected = {"type": "pi",
                     "proj": ["Nombre", "Edad"],
--- tests/SqlJson_Test_SELECT.py
+++ tests/SqlJson_Test_SELECT.py
@@ -2,8 +2,28 @@ import unittest
 from src.SQL_To_JSON import Sql_To_Json as sqlJSON
 
 class SqlJson_Test_SELECT(unittest.TestCase):
+    """
+    Class to test transformations from SQL to relational algebra, focusing on SELECT various attributes
+
+    Methods
+    _______
+    test_oneSELECT
+        Test of SELECT one attribute
+
+    test_twoSELECT
+        Test of SELECT two attributes
+
+    test_fiveSELECT
+        Test of SELECT five attributes
+    """
 
     def test_oneSELECT(self):
+        """
+        Test of SELECT one attribute
+
+        Original query: SELECT Nombre FROM Persona
+        """
+
         res = sqlJSON.parse_Sql_To_Json("SELECT Nombre FROM Persona")
         expected = {"type": "pi",
                     "proj": ["Nombre"],
@@ -14,6 +34,12 @@ class SqlJson_Test_SELECT(unittest.TestCase):
         self.assertEqual(res, expected)
 
     def test_twoSELECT(self):
+        """
+        Test of SELECT two attributes
+
+        Original query: SELECT Nombre, Edad FROM Persona
+        """
+
         res = sqlJSON.parse_Sql_To_Json("SELECT Nombre, Edad FROM Persona")
         expected = {"type": "pi",
                     "proj": ["Nombre", "Edad"],
@@ -24,6 +50,12 @@ class SqlJson_Test_SELECT(unittest.TestCase):
         self.assertEqual(res, expected)
 
     def test_fiveSELECT(self):
+        """
+        Test of SELECT five attributes
+
+        Original query: SELECT Nombre, Ap1, Ap2, Edad, Telefono FROM Persona
+        """
+
         res = sqlJSON.parse_Sql_To_Json("SELECT Nombre, Ap1, Ap2, Edad, Telefono FROM Persona")
         expected = {"type": "pi",
                     "proj": ["Nombre", "Ap1", "Ap2", "Edad", "Telefono"],
