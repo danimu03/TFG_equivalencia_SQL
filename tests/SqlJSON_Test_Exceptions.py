d059d1cd3dc19de427bbf4eacc882c347dd96e94
sergiova97 <svillanuevaa97@gmail.com>
2022-02-28 11:25:15 +0100

    Update SqlJSON_Test_Exceptions.py

--- tests/SqlJSON_Test_Exceptions.py
+++ tests/SqlJSON_Test_Exceptions.py
@@ -4,33 +4,31 @@ import src.SQL_To_JSON.Sql_To_Json as sqlJSON
 
 class SqlJson_Test_Exceptions(unittest.TestCase):
 
+    def test_neqException2(self):
+        self.assertRaises(sqlJSON.ErrorSqlQuery, sqlJSON.parse_Sql_To_Json, "SELECT Nombre, direccion FROM usuario WHERE pais != \"España\"")
+
     def test_neqException(self):
-        with self.assertRaises(sqlJSON.ErrorSqlQuery):
-            sqlJSON.parse_Sql_To_Json("SELECT Nombre, direccion FROM usuario WHERE pais != \"España\"")
+        self.assertRaises(sqlJSON.ErrorSqlQuery, sqlJSON.parse_Sql_To_Json, "SELECT Nombre, direccion FROM usuario WHERE pais != \"España\"")
 
     def test_ltException(self):
-        with self.assertRaises(sqlJSON.ErrorSqlQuery):
-            sqlJSON.parse_Sql_To_Json("SELECT Nombre, direccion FROM usuario WHERE id < 3456")
+        self.assertRaises(sqlJSON.ErrorSqlQuery, sqlJSON.parse_Sql_To_Json, "SELECT Nombre, direccion FROM usuario WHERE id < 3456")
 
     def test_gtException(self):
-        with self.assertRaises(sqlJSON.ErrorSqlQuery):
-            sqlJSON.parse_Sql_To_Json("SELECT Nombre, direccion FROM usuario WHERE id > 3456")
+        self.assertRaises(sqlJSON.ErrorSqlQuery, sqlJSON.parse_Sql_To_Json, "SELECT Nombre, direccion FROM usuario WHERE id < 3456")
 
     def test_lteException(self):
-        with self.assertRaises(sqlJSON.ErrorSqlQuery):
-            sqlJSON.parse_Sql_To_Json("SELECT Nombre, direccion FROM usuario WHERE id <= 3456")
+        self.assertRaises(sqlJSON.ErrorSqlQuery, sqlJSON.parse_Sql_To_Json, "SELECT Nombre, direccion FROM usuario WHERE id <= 3456")
 
     def test_gteException(self):
-        with self.assertRaises(sqlJSON.ErrorSqlQuery):
-            sqlJSON.parse_Sql_To_Json("SELECT Nombre, direccion FROM usuario WHERE pais >= \"España\"")
+        self.assertRaises(sqlJSON.ErrorSqlQuery, sqlJSON.parse_Sql_To_Json, "SELECT Nombre, direccion FROM usuario WHERE pais >= \"España\"")
 
     def test_orException(self):
-        with self.assertRaises(sqlJSON.ErrorSqlQuery):
-            sqlJSON.parse_Sql_To_Json("SELECT Nombre, direccion FROM usuario WHERE pais = \"España\" OR id = 1")
+        self.assertRaises(sqlJSON.ErrorSqlQuery, sqlJSON.parse_Sql_To_Json, "SELECT Nombre, direccion FROM usuario WHERE pais = \"España\" OR id = 1")
 
     def test_groupbyException(self):
-        with self.assertRaises(sqlJSON.ErrorSqlQuery):
-            sqlJSON.parse_Sql_To_Json("SELECT Nombre, direccion FROM usuario, persona, movil WHERE pais = \"España\" and num = 1 GROUP BY name")
+        self.assertRaises(sqlJSON.ErrorSqlQuery, sqlJSON.parse_Sql_To_Json, "SELECT Nombre, direccion FROM usuario, persona, movil WHERE pais = \"España\" and num = 1 GROUP BY name")
+
+
 
 
 
